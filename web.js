eb3960c4ed3bcb95d536b4f7f69972a52c03c96c
Tiago Henriques <trinosauro@gmail.com>
2013-07-10 22:06:11 +0200

    Fixed jslint warnings

--- web.js
+++ web.js
@@ -1,15 +1,18 @@
+/*jslint node: true, indent: 4, vars: true */
+"use strict";
+
 var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  var fileBuffer = fs.readFileSync('index.html');
-  var str = fileBuffer.toString();
-  response.send(str);
+app.get('/', function (request, response) {
+    var fileBuffer = fs.readFileSync('index.html');
+    var str = fileBuffer.toString();
+    response.send(str);
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port, function () {
+    console.log("Listening on " + port);
 });
