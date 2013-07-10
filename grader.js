f352d8603df421249a721fa96f7bd3974d3a9582
Tiago Henriques <trinosauro@gmail.com>
2013-07-10 23:06:34 +0200

    Fixed jslint indentation errors

--- grader.js
+++ grader.js
@@ -88,9 +88,9 @@ var clone = function (fn) {
 if (require.main === module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json',
-        clone(assertFileExists), CHECKSFILE_DEFAULT)
+            clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html',
-        clone(assertFileExists), HTMLFILE_DEFAULT)
+            clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-u, --url <url>', 'URL')
         .parse(process.argv);
     if (program.url) {
