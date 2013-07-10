85595a51af6a9ddce7d59a00e070dc2ab7df2079
Tiago Henriques <trinosauro@gmail.com>
2013-07-10 22:47:18 +0200

    Implemented option to grade URL

--- grader.js
+++ grader.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 /*jslint node: true, indent: 4, vars: true */
 "use strict";
 
@@ -27,6 +28,9 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var sys = require('util');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -61,6 +65,20 @@ var checkHtmlFile = function (htmlfile, checksfile) {
     return out;
 };
 
+var checkUrl = function (urlText, checksfile) {
+    var ii;
+    var $ = cheerio.load(urlText);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for (ii in checks) {
+        if (checks.hasOwnProperty(ii)) {
+            var present = $(checks[ii]).length > 0;
+            out[checks[ii]] = present;
+        }
+    }
+    return out;
+};
+
 var clone = function (fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -69,12 +87,33 @@ var clone = function (fn) {
 
 if (require.main === module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json',
+        clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html',
+        clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.checks) console.log('  - checks ' + program.checks);
+    if (program.url) {
+        console.log('  - url ' + program.url);
+
+        rest.get(program.url).on('complete', function (result) {
+            if (result instanceof Error) {
+                sys.puts('Error: ' + result.message);
+                this.retry(5000); // try again after 5 sec  
+            } else {
+                //                console.log(typeof result);
+                var checkJson = checkUrl(result, program.checks);
+                var outJson = JSON.stringify(checkJson, null, 4);
+                console.log(outJson);
+            }
+        });
+    } else if (program.file) {
+        console.log('  - file ' + program.file);
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
