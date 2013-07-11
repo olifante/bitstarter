f00168adf97918177e0bb22010405b159f83a79b
Tiago Henriques <trinosauro@gmail.com>
2013-07-11 10:29:43 +0200

    Reindented with 2 spaces

--- grader.js
+++ grader.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-/*jslint node: true, indent: 4, vars: true */
+/*jslint node: true, indent: 2, vars: true */
 "use strict";
 
 /*
@@ -35,80 +35,80 @@ var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function (infile) {
-    var instr = infile.toString();
-    if (!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-    }
-    return instr;
+  var instr = infile.toString();
+  if (!fs.existsSync(instr)) {
+    console.log("%s does not exist. Exiting.", instr);
+    process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+  }
+  return instr;
 };
 
 var cheerioHtmlFile = function (htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+  return cheerio.load(fs.readFileSync(htmlfile));
 };
 
 var loadChecks = function (checksfile) {
-    return JSON.parse(fs.readFileSync(checksfile));
+  return JSON.parse(fs.readFileSync(checksfile));
 };
 
 var checkHtmlFile = function (htmlfile, checksfile) {
-    var ii;
-    var $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for (ii in checks) {
-        if (checks.hasOwnProperty(ii)) {
-            var present = $(checks[ii]).length > 0;
-            out[checks[ii]] = present;
-        }
+  var ii;
+  var $ = cheerioHtmlFile(htmlfile);
+  var checks = loadChecks(checksfile).sort();
+  var out = {};
+  for (ii in checks) {
+    if (checks.hasOwnProperty(ii)) {
+      var present = $(checks[ii]).length > 0;
+      out[checks[ii]] = present;
     }
-    return out;
+  }
+  return out;
 };
 
 var checkUrl = function (urlText, checksfile) {
-    var ii;
-    var $ = cheerio.load(urlText);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for (ii in checks) {
-        if (checks.hasOwnProperty(ii)) {
-            var present = $(checks[ii]).length > 0;
-            out[checks[ii]] = present;
-        }
+  var ii;
+  var $ = cheerio.load(urlText);
+  var checks = loadChecks(checksfile).sort();
+  var out = {};
+  for (ii in checks) {
+    if (checks.hasOwnProperty(ii)) {
+      var present = $(checks[ii]).length > 0;
+      out[checks[ii]] = present;
     }
-    return out;
+  }
+  return out;
 };
 
 var clone = function (fn) {
-    // Workaround for commander.js issue.
-    // http://stackoverflow.com/a/6772648
-    return fn.bind({});
+  // Workaround for commander.js issue.
+  // http://stackoverflow.com/a/6772648
+  return fn.bind({});
 };
 
 if (require.main === module) {
-    program
-        .option('-c, --checks <check_file>', 'Path to checks.json',
-            clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html',
-            clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <url>', 'URL')
-        .parse(process.argv);
-    if (program.url) {
-        rest.get(program.url).on('complete', function (result) {
-            if (result instanceof Error) {
-                sys.puts('Error: ' + result.message);
-                this.retry(5000); // try again after 5 sec  
-            } else {
-                var checkJson = checkUrl(result, program.checks);
-                var outJson = JSON.stringify(checkJson, null, 4);
-                console.log(outJson);
-            }
-        });
-    } else if (program.file) {
-        var checkJson = checkHtmlFile(program.file, program.checks);
+  program
+    .option('-c, --checks <check_file>', 'Path to checks.json',
+      clone(assertFileExists), CHECKSFILE_DEFAULT)
+    .option('-f, --file <html_file>', 'Path to index.html',
+      clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url <url>', 'URL')
+    .parse(process.argv);
+  if (program.url) {
+    rest.get(program.url).on('complete', function (result) {
+      if (result instanceof Error) {
+        sys.puts('Error: ' + result.message);
+        this.retry(5000); // try again after 5 sec
+      } else {
+        var checkJson = checkUrl(result, program.checks);
         var outJson = JSON.stringify(checkJson, null, 4);
         console.log(outJson);
-    }
+      }
+    });
+  } else if (program.file) {
+    var checkJson = checkHtmlFile(program.file, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+  }
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+  exports.checkHtmlFile = checkHtmlFile;
 }
--- web.js
+++ web.js
@@ -1,4 +1,4 @@
-/*jslint node: true, indent: 4, vars: true */
+/*jslint node: true, indent: 2, vars: true */
 "use strict";
 
 var express = require('express');
@@ -7,12 +7,12 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function (request, response) {
-    var fileBuffer = fs.readFileSync('index.html');
-    var str = fileBuffer.toString();
-    response.send(str);
+  var fileBuffer = fs.readFileSync('index.html');
+  var str = fileBuffer.toString();
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function () {
-    console.log("Listening on " + port);
+  console.log("Listening on " + port);
 });
